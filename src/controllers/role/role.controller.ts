4e1f29d133e8f6f4e0792166e2aa03dc3593a476
Aamirkhan <aamir.nawaz990@gmail.com>
2024-08-11 19:49:12 +0500

    role - endpoint created for roles

--- src/controllers/role/role.controller.ts
+++ src/controllers/role/role.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { RoleService } from '../../services/role/role.service';
+import { CreateRoleDto } from '../../dto/role/create-role.dto';
+import { UpdateRoleDto } from '../../dto/role/update-role.dto';
 
 @Controller('role')
-export class RoleController {}
+export class RoleController {
+  constructor(private readonly roleService: RoleService) {}
+
+  @Post()
+  async create(@Body() createRoleDto: CreateRoleDto) {
+    return this.roleService.create(createRoleDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.roleService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    return this.roleService.findOne(id);
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: number, @Body() updateRoleDto: UpdateRoleDto) {
+    return this.roleService.update(id, updateRoleDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: number) {
+    return this.roleService.remove(id);
+  }
+}
--- src/services/role/role.service.ts
+++ src/services/role/role.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Role } from '../../entities/Role.entity';
+import { CreateRoleDto } from '../../dto/role/create-role.dto';
+import { UpdateRoleDto } from '../../dto/role/update-role.dto';
 
 @Injectable()
-export class RoleService {}
+export class RoleService {
+  constructor(
+    @InjectRepository(Role)
+    private readonly roleRepository: Repository<Role>,
+  ) {}
+
+  async create(createRoleDto: CreateRoleDto): Promise<Role> {
+    const role = this.roleRepository.create(createRoleDto);
+    return this.roleRepository.save(role);
+  }
+
+  async findAll(): Promise<Role[]> {
+    return this.roleRepository.find();
+  }
+
+  async findOne(id: number): Promise<Role> {
+    const role = await this.roleRepository.findOneBy({ role_id: id });
+    if (!role) {
+      throw new NotFoundException(`Role with ID ${id} not found`);
+    }
+    return role;
+  }
+
+  async update(id: number, updateRoleDto: UpdateRoleDto): Promise<Role> {
+    const role = await this.findOne(id);
+    Object.assign(role, updateRoleDto);
+    return this.roleRepository.save(role);
+  }
+
+  async remove(id: number): Promise<void> {
+    const role = await this.findOne(id);
+    await this.roleRepository.remove(role);
+  }
+}
