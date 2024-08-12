6c5d75643f00ee62dd4c19c3aa83e7fc2d4dfc1e
Aamirkhan <aamir.nawaz990@gmail.com>
2024-08-12 12:18:08 +0500

    resolved dependencies issue in booking service

--- src/controllers/booking/booking.module.ts
+++ src/controllers/booking/booking.module.ts
@@ -4,11 +4,12 @@ import { Booking } from 'src/entities/Booking.entity';
 import { BookingService } from 'src/services/booking/booking.service';
 import { BookingController } from './booking.controller';
 import { UserModule } from '../user/user.module';
+import { User } from 'src/entities/User.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Booking])],
+  imports: [TypeOrmModule.forFeature([Booking, User])],
   providers: [BookingService],
   controllers: [BookingController],
-  exports: [BookingService],
+  // exports: [BookingService],
 })
 export class BookingModule {}
--- src/services/booking/booking.service.ts
+++ src/services/booking/booking.service.ts
@@ -2,8 +2,8 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Booking } from '../../entities/booking.entity';
-// import { CreateBookingDto } from '../../dto/booking/create-booking.dto';
-// import { UpdateBookingDto } from '../../dto/booking/update-booking.dto';
+import { CreateBookingDto } from '../../dto/booking/create-booking.dto';
+import { UpdateBookingDto } from '../../dto/booking/update-booking.dto';
 import { User } from 'src/entities/User.entity';
 
 @Injectable()
@@ -11,25 +11,26 @@ export class BookingService {
   constructor(
     @InjectRepository(Booking)
     private readonly bookingRepository: Repository<Booking>,
-    // private readonly userRepository: Repository<User>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
   ) {}
 
-  // async create(createBookingDto: CreateBookingDto): Promise<Booking> {
-  //   const user = await this.userRepository.findOne({
-  //     where: { user_id: createBookingDto.user },
-  //   });
+  async create(createBookingDto: CreateBookingDto): Promise<Booking> {
+    const user = await this.userRepository.findOne({
+      where: { user_id: createBookingDto.user },
+    });
 
-  //   if (!user) {
-  //     throw new NotFoundException('User not found');
-  //   }
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
 
-  //   const booking = this.bookingRepository.create({
-  //     ...createBookingDto,
-  //     user: user,
-  //   });
+    const booking = this.bookingRepository.create({
+      ...createBookingDto,
+      user: user,
+    });
 
-  //   return this.bookingRepository.save(booking);
-  // }
+    return this.bookingRepository.save(booking);
+  }
 
   async findAll(): Promise<Booking[]> {
     return this.bookingRepository.find({
@@ -44,46 +45,46 @@ export class BookingService {
     });
   }
 
-  // async update(
-  //   id: number,
-  //   updateBookingDto: UpdateBookingDto,
-  // ): Promise<Booking> {
-  //   const bookingToUpdate = await this.bookingRepository.findOne({
-  //     where: { booking_id: id },
-  //   });
+  async update(
+    id: number,
+    updateBookingDto: UpdateBookingDto,
+  ): Promise<Booking> {
+    const bookingToUpdate = await this.bookingRepository.findOne({
+      where: { booking_id: id },
+    });
 
-  //   if (!bookingToUpdate) {
-  //     throw new NotFoundException('Booking not found');
-  //   }
+    if (!bookingToUpdate) {
+      throw new NotFoundException('Booking not found');
+    }
 
-  //   if (updateBookingDto.user) {
-  //     const user = await this.userRepository.findOne({
-  //       where: { user_id: updateBookingDto.user },
-  //     });
-  //     if (!user) {
-  //       throw new NotFoundException('User not found');
-  //     }
-  //     bookingToUpdate.user = user;
-  //   }
+    if (updateBookingDto.user) {
+      const user = await this.userRepository.findOne({
+        where: { user_id: updateBookingDto.user },
+      });
+      if (!user) {
+        throw new NotFoundException('User not found');
+      }
+      bookingToUpdate.user = user;
+    }
 
-  //   if (updateBookingDto.service) {
-  //     bookingToUpdate.service = updateBookingDto.service;
-  //   }
+    if (updateBookingDto.service) {
+      bookingToUpdate.service = updateBookingDto.service;
+    }
 
-  //   if (updateBookingDto.status) {
-  //     bookingToUpdate.status = updateBookingDto.status;
-  //   }
+    if (updateBookingDto.status) {
+      bookingToUpdate.status = updateBookingDto.status;
+    }
 
-  //   if (updateBookingDto.created_at) {
-  //     bookingToUpdate.created_at = updateBookingDto.created_at;
-  //   }
+    if (updateBookingDto.created_at) {
+      bookingToUpdate.created_at = updateBookingDto.created_at;
+    }
 
-  //   if (updateBookingDto.updated_at) {
-  //     bookingToUpdate.updated_at = updateBookingDto.updated_at;
-  //   }
+    if (updateBookingDto.updated_at) {
+      bookingToUpdate.updated_at = updateBookingDto.updated_at;
+    }
 
-  //   return this.bookingRepository.save(bookingToUpdate);
-  // }
+    return this.bookingRepository.save(bookingToUpdate);
+  }
 
   async remove(id: number): Promise<void> {
     await this.bookingRepository.delete(id);
