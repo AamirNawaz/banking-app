6c3cdb2187fae1eaa865509de6dff38d351cd629
Aamirkhan <aamir.nawaz990@gmail.com>
2024-08-12 20:28:17 +0500

    Applied guard on review and roles routes

--- src/controllers/review/review.controller.ts
+++ src/controllers/review/review.controller.ts
@@ -6,32 +6,42 @@ import {
   Param,
   Put,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { ReviewService } from '../../services/review/review.service';
 import { CreateReviewDto } from '../../dto/review/create-review.dto';
 import { UpdateReviewDto } from '../../dto/review/update-review.dto';
 import { Review } from '../../entities/Review.entity';
+import { Roles } from 'src/guard/roles.decorator';
+import { UserGuard } from 'src/guard/user.guard';
 
 @Controller('reviews')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
   @Post()
+  @UseGuards(UserGuard)
+  @Roles('customer')
   create(@Body() createReviewDto: CreateReviewDto): Promise<Review> {
     return this.reviewService.create(createReviewDto);
   }
 
   @Get()
+  @UseGuards(UserGuard)
+  @Roles('customer')
   findAll(): Promise<Review[]> {
     return this.reviewService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(UserGuard)
   findOne(@Param('id') id: number): Promise<Review> {
     return this.reviewService.findOne(id);
   }
 
   @Put(':id')
+  @Roles('customer')
+  @UseGuards(UserGuard)
   update(
     @Param('id') id: number,
     @Body() updateReviewDto: UpdateReviewDto,
@@ -40,6 +50,8 @@ export class ReviewController {
   }
 
   @Delete(':id')
+  @Roles('customer')
+  @UseGuards(UserGuard)
   remove(@Param('id') id: number): Promise<void> {
     return this.reviewService.remove(id);
   }
--- src/controllers/role/role.controller.ts
+++ src/controllers/role/role.controller.ts
@@ -6,32 +6,41 @@ import {
   Param,
   Delete,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { RoleService } from '../../services/role/role.service';
 import { CreateRoleDto } from '../../dto/role/create-role.dto';
 import { UpdateRoleDto } from '../../dto/role/update-role.dto';
 import { Role } from 'src/entities/Role.entity';
+import { Roles } from 'src/guard/roles.decorator';
+import { UserGuard } from 'src/guard/user.guard';
 
 @Controller('role')
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
   @Post()
+  @UseGuards(UserGuard)
+  @Roles('admin')
   create(@Body() createRoleDto: CreateRoleDto): Promise<Role> {
     return this.roleService.create(createRoleDto);
   }
 
   @Get()
+  @UseGuards(UserGuard)
   findAll(): Promise<Role[]> {
     return this.roleService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(UserGuard)
   findOne(@Param('id') id: number): Promise<Role> {
     return this.roleService.findOne(id);
   }
 
   @Put(':id')
+  @UseGuards(UserGuard)
+  @Roles('admin')
   update(
     @Param('id') id: number,
     @Body() updateRoleDto: UpdateRoleDto,
@@ -40,6 +49,8 @@ export class RoleController {
   }
 
   @Delete(':id')
+  @UseGuards(UserGuard)
+  @Roles('admin')
   async remove(@Param('id') id: number): Promise<void> {
     return this.roleService.remove(id);
   }
