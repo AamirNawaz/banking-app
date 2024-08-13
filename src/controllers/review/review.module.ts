22cf75eb2c27e1b9256a521b446501ca096afbb5
Aamirkhan <aamir.nawaz990@gmail.com>
2024-08-13 22:27:25 +0500

    Bug - Fixed review depencey issue in review service and review module

--- src/controllers/review/review.module.ts
+++ src/controllers/review/review.module.ts
@@ -4,9 +4,10 @@ import { Review } from 'src/entities/Review.entity';
 import { ReviewService } from 'src/services/review/review.service';
 import { ReviewController } from './review.controller';
 import { Booking } from 'src/entities/Booking.entity';
+import { User } from 'src/entities/User.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Review, Booking])],
+  imports: [TypeOrmModule.forFeature([Review, Booking, User])],
   providers: [ReviewService],
   controllers: [ReviewController],
 })
